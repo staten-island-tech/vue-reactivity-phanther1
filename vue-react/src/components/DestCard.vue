3f90e39daab3e7fe706f7e28c3f283168b076c65
phanther1 <144461089+phanther1@users.noreply.github.com>
2024-02-05 10:14:41 -0500

    fd

---
+++ vue-react/src/components/DestCard.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h2>{{  Destination.name }}</h2>
+        <img :src="Destination.img" alt="">
+        <h3>{{ @clicked }}</h3>
+        <button>Click here</button>
+    </div>
+</template>
+
+<script setup>
+import {ref} from "vue";
+const props = defineProps({
+    Destination: Object,
+});
+
+//clicker logic
+const clicked = ref(0);
+function increment(){
+    clicked.value++;
+}
+</script>
+
+<style scoped>
+/* h1 {
+    color: red;  //only affects this file
+}
+ */
+
+ img {
+    width: 300px;
+    height: 300px;
+ }
+</style>
